cd1c7feddacbbb99ddab2f1b94c47fcfe6532260
KiJeongg <rlwjd282@naver.com>
2025-03-12 21:24:59 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,17 +1,10 @@
-//Q1. 3의 배수의 합 구하기
-//while 문을 사용해 1 부터 1000 까지의 자연수 중 3 의 배수의 합을 구해 보자.
-
-
+//"Q1 : 전과목 평균 점수를 구해보자"
+//     "국어 80, 영어 75, 수학 55"
 public class Main {
     public static void main(String[] args) {
-        int result = 0;
-        int i = 1;
-        while (i<1000) {
-            if(i%3==0){
-                result +=i;
-            }
-            i +=1;
-        }
-        System.out.println(result);
+        int a = 80;
+        int b = 75;
+        int c = 55;
+        System.out.println((a+b+c)/3);
     }
 }
--- Main4.java
+++ Main4.java
@@ -1,15 +1,10 @@
-//Q4. 평균 점수 구하기
-//70, 60, 55, 75, 95, 90, 80, 80, 85, 100 점수가 이렇게 있고
-//for each 문을 사용하여 A 학급의 평균 점수를 구해 보자
+//"Q4 : 원하는 숫자 출력하기
+// 주민 등록 번호 뒷자리의 첫 번째 숫자는 성별을 나타낸다. 첫 번째 숫자가 1 이면 남자를, 2 이면 여자를 의미한다.
+// 홍길동 씨의 주민 등록 번호 881120‑1068234 에서 성별을 나타내는 숫자를 출력해 보자.
 
 public class Main4 {
-    public static void main (String[] args) {
-        int[] marks = {70, 60, 55, 75, 95, 90, 80, 85, 100};
-        int total = 0;
-        for (int mark:marks) {
-            total +=mark;
-        }
-        float average = (float) total / marks.length;
-        System.out.println(average);
+    public static void main(String[] args) {
+        String pin = "881120-1068234";
+        System.out.println(pin.charAt(7));
     }
 }
